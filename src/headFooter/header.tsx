7e3777da98a63000bb2afe0242d605c015547fb9
gigi <dobrerobert441@gmail.com>
2025-03-11 13:32:44 +0200

    ts coversion & other changes

---
+++ src/headFooter/header.tsx
@@ -0,0 +1,158 @@
+import { useState, useEffect } from "react";
+import SkillsUI from "../SkillsUI";
+import "./header.css";
+
+interface HeaderProps {
+  bgToggle: () => void;
+  bgSwitch: string;
+}
+
+const specialArray: string[] = ["!", "!!", "?", "??", "" , "Umm", "Friend", "Innovator", "Visitor", "Developer", "Passionate"];
+
+const getRandomItemFromArray = (array: string[]): string => {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+
+const Header: React.FC<HeaderProps> = ({ bgToggle, bgSwitch }) => {
+  const [specialCharacters, setSpecialCharacters] = useState<string>(getRandomItemFromArray(specialArray));
+  const [glitch, setGlitch] = useState<boolean>(false);
+  const [color, setColor] = useState<string>("white");
+  const [color2, setColor2] = useState<string>("black");
+  const [backColor, setBackColor] = useState<string>("");
+  const [btnBack, setBtnBack] = useState<string>("");
+  const [openSkills, setOpenSkills] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setOpenSkills((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSpecialCharacters(getRandomItemFromArray(specialArray));
+      setGlitch(Math.random() > 0.7);
+    }, 1200);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const handleDblClick = () => {
+      setColor("black");
+      setColor2("white");
+      setBackColor("hsla(0, 0%, 0%, 0.2)");
+      setBtnBack("hsl(0, 0%, 10%)");
+
+      setTimeout(() => {
+        setColor("white");
+        setColor2("black");
+        setBackColor("hsla(0, 0%, 100%, 0.2)");
+        setBtnBack("white");
+      }, 500);
+    };
+
+    document.addEventListener("dblclick", handleDblClick);
+    return () => document.removeEventListener("dblclick", handleDblClick);
+  }, []);
+
+  return (
+    <>
+      <header className="header-container">
+        <h1 
+          id="welcome" 
+          className={`darkSwitchColor transition-all duration-300 ease-in-out ${glitch ? "glitch" : ""}`}
+        >
+          Welcome 
+          <span className="special"> {specialCharacters}</span>
+        </h1>
+        <div onClick={bgToggle} className={`bg-slider ${bgSwitch}`}>
+          <div className="switch"></div><br/>
+          <h5>{bgSwitch === "on" ? "bg" : "no-bg"}</h5>
+        </div>
+      </header>
+
+      <div className="moreAbout">
+        <div className="litteTalk">
+          <b>
+            <span id="me" className="darkSwitchColor">
+              A highly passionate web Developer.
+            </span>
+            <br />
+            <span id="me" className="darkSwitchColor">Dobre Robert</span>
+          </b>
+          <br />
+          <div className="tech-icons">
+            {[
+              { link: "https://www.w3.org/html/", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg", alt: "HTML5" },
+              { link: "https://www.w3schools.com/css/", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg", alt: "CSS3" },
+              { link: "https://sass-lang.com", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg", alt: "SASS" },
+              { link: "https://tailwindcss.com", icon: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg", alt: "Tailwind" },
+              { link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg", alt: "JavaScript" },
+              { link: "https://www.typescriptlang.org/", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg", alt: "TypeScript" },
+              { link: "https://reactjs.org/", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg", alt: "React" },
+            ].map((tech, index) => (
+              <a key={index} href={tech.link} target="_blank" className="Recources" rel="noreferrer">
+                <img src={tech.icon} alt={tech.alt} />
+              </a>
+            ))}
+          </div>
+          <br />
+          <div className="skillInfo">
+            <a href="https://raw.githubusercontent.com/GIGIsOtherStuff/mainWebMedia/main/AppImages/resume.pdf" download="resume.pdf">
+              <button className="resume">My Resume</button>
+            </a>
+            <button onClick={handleClick} className="resume">
+              Full Skills
+            </button>
+          </div>
+        </div>
+        <div className="vidContainer">
+          <iframe
+            id="video"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/T3e0iaCirdI"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+
+      <style>{`
+        .darkSwitchColor {
+          color: ${color};
+          transition: color 0.5s ease-in-out;
+        }
+
+        .glitch {
+          text-shadow: 2px 0 red, -2px 0 blue;
+          animation: glitch-effect 0.3s infinite alternate;
+        }
+
+        @keyframes glitch-effect {
+          0% {
+            transform: translateX(-2px);
+          }
+          100% {
+            transform: translateX(2px);
+          }
+        }
+
+        .darkSwitchBack {
+          background-color: ${backColor};
+          transition: background-color 0.5s ease-in-out;
+        }
+
+        .visitBtn {
+          color: ${color2};
+          background-color: ${btnBack};
+        }
+      `}</style>
+
+      <SkillsUI skillsOpen={openSkills} />
+    </>
+  );
+};
+
+export default Header;
---
+++ src/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./headFooter/header";
+import NavBar from "./navBar/nav";
+import MainProjects from "./main-projects/mainProjects";
+import MoreProjects from "./More-apps/moreProjects";
+import Welcome from "./welcome";
+import Footer from "./headFooter/footer";
+import SkillsUI from "./SkillsUI";
+import "./index.css";
+import "./Mobile.css";
+
+const bgVidWebm = "https://raw.githubusercontent.com/GIGIsOtherStuff/mainWebMedia/main/AppImages/backgrounds/webBg-vmake.webm";
+const bgVidMp4 = "https://raw.githubusercontent.com/GIGIsOtherStuff/mainWebMedia/main/AppImages/backgrounds/webBg-vmake.mp4";
+
+// VideoBackground component
+const VideoBackground: React.FC = () => (
+  <video className="video-background" autoPlay loop muted playsInline>
+    <source src={bgVidWebm} type="video/webm" />
+    <source src={bgVidMp4} type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+);
+
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+  const [dontShowAgain, setDontShowAgain] = useState<boolean>(() => {
+    return JSON.parse(localStorage.getItem("dontShow") ?? "false");
+  });
+
+  const [bgSwitch, setBgSwitch] = useState<string>(() => {
+    const savedBgSwitch = localStorage.getItem("bgSwitch");
+    return savedBgSwitch ? savedBgSwitch : "on";
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      localStorage.setItem("dontShow", JSON.stringify(dontShowAgain));
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [dontShowAgain]);
+
+  const bgToggle = () => {
+    const newSwitchState = bgSwitch === "on" ? "off" : "on";
+    setBgSwitch(newSwitchState);
+    localStorage.setItem("bgSwitch", newSwitchState);
+  };
+
+  return (
+    <>
+      {bgSwitch === "on" && <VideoBackground />}
+      {!dontShowAgain && <Welcome setDontShowAgain={setDontShowAgain} dontShowAgain={dontShowAgain} />}
+      <div className="opacity">
+        <NavBar />
+        <Header bgToggle={bgToggle} bgSwitch={bgSwitch} />
+        <MainProjects />
+        <MoreProjects />
+        <Footer />
+        {/* <SkillsUI skillsOpen={true}/> */}
+      </div>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
